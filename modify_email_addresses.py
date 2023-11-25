357616230f6d077cdb02c44ef56821aa76487ab7
MarioNeugebauer <48792182+MarioNeugebauer@users.noreply.github.com>
2023-11-25 14:14:46 +0100

    added option to generate one comma separated line in output file

--- modify_email_addresses.py
+++ modify_email_addresses.py
@@ -1,18 +1,34 @@
-def generate_emails(input_file, output_file, keep_original_address_in_list):
+# input_file
+# output_file
+# keep_original_address_in_list
+# make_csl make only one line with comma separated emails
+def generate_emails(input_file, output_file, keep_original_address_in_list, make_csl):
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-        for line in infile:
+        total_lines = sum(1 for _ in infile)
+        infile.seek(0)  # Zurücksetzen des Dateizeigers auf den Anfang der Datei
+        for index, line in enumerate(infile, start=1):
+        # for line in infile:
             email = line.strip()
             if '@' in email:
                 username, domain = email.split('@')
                 new_email1 = f"{username}.e1@{domain}"
                 new_email2 = f"{username}.e2@{domain}"
-                if keep_original_address_in_list:
-                    outfile.write(f"{email}\n")
-                outfile.write(f"{new_email1}\n")
-                outfile.write(f"{new_email2}\n")
+                if make_csl:
+                    if keep_original_address_in_list:
+                        outfile.write(f"{email}, ")
+                    outfile.write(f"{new_email1}, ")
+                    outfile.write(f"{new_email2}")
+                    if index < total_lines:
+                        outfile.write(f", ")
+
+                else:
+                    if keep_original_address_in_list:
+                        outfile.write(f"{email}\n")
+                    outfile.write(f"{new_email1}\n")
+                    outfile.write(f"{new_email2}\n")
 
 # Example
 input_filename = '/mnt/c/daten/temp/schule/input_emails.txt'  # File with email addresses as input
-output_filename = '/mnt/c/daten/temp/schule/output_emails.txt'  # File to write the email addresses 
+output_filename = '/mnt/c/daten/temp/schule/output_emails.txt'  # File to write the email addresses
 
-generate_emails(input_filename, output_filename, True)
+generate_emails(input_filename, output_filename, False, True)
